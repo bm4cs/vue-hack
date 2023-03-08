2f81b33dc4f6f2778d1fd816e6c9cc4d84801c2b
Ben Simmonds <ben@bencode.net>
2023-03-08 21:10:40 +1100

    vue 3 fundamentals on frontendmasters

---
+++ 02-vuetools/sandbox/src/components/Counter.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  data: () => ({
+    count: 10,
+    incrementAmount: 8
+  }),
+  methods: {
+    incrementCount() {
+      this.count += this.incrementAmount
+    }
+  },
+  watch: {
+    count(newValue, oldValue) {
+      console.log('newValue: ' + newValue + ' oldValue: ' + oldValue)
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>Counter</h1>
+  <p>{{ count }}</p>
+  <button v-on:click="incrementCount">Increment Count</button>
+  <h1>{{ incrementAmount }}</h1>
+  <div>
+    <label for="incrementAmount">Increment by:</label>
+    <input type="number" v-model="incrementAmount" />
+  </div>
+</template>
